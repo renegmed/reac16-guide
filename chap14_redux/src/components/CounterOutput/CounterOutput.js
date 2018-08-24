ecee75a5d25004ec9a3046f144fe44f6ce3ef64d
rene <renegmed@yahoo.com>
2018-08-24 13:41:43 -0400

    14 showed how global redux props used in another class

--- chap14_redux/src/components/CounterOutput/CounterOutput.js
+++ chap14_redux/src/components/CounterOutput/CounterOutput.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import './CounterOutput.css';
+import "./CounterOutput.css";
 
-const counterOutput = (props) => (
-    <div className="CounterOutput">
-        Current Counter: {props.value}
-    </div>
-);
+class counterOutput extends Component {
+  render() {
+    return <div className="CounterOutput">Current Counter: {this.props.ctr}</div>;
+  }
+}
 
-export default counterOutput;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter, // state.counter is updated by /store/reducer.js
+    storedResults: state.results // state.results is updated by /store/reducer.js
+  };
+};
+
+export default connect(mapStateToProps)(counterOutput);
