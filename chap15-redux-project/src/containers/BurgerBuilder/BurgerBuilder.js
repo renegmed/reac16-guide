b53ac5e1abe0d7eda69c6a87200720b59aa9ee3a
rene <renegmed@yahoo.com>
2018-08-23 20:42:53 -0400

    15 adjusted checkout and contact data, more redux connects

--- chap15-redux-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ chap15-redux-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -55,22 +55,10 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+       this.props.history.push('/checkout');        
     }
 
-    render () {
-        console.log("+++++++ BurgerBuilder.render() +++++++++++")
+    render () {        
         const disabledInfo = {
             ...this.props.ings
         };
